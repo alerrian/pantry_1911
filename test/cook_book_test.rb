d5cd4c92042b15d749e90a9ac4f4ec46687fe695
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-08 11:30:27 -0700

    Add test for exists and attributes

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -7,5 +7,16 @@ require_relative '../lib/cook_book'
 class CookBookTest < Minitest::Test
   def setup
     @cookbook = CookBook.new
+    @ingredient1 = Ingredient.new('Cheese', 'C', 100)
+    @ingredient2 = Ingredient.new('Macaroni', 'oz', 30)
+    @recipe1 = Recipe.new('Mac and Cheese')
+  end
+
+  def test_a_cookbook_exists
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_a_cook_book_hash_attributes
+    assert_equal [], @cookbook.recipes
   end
 end
