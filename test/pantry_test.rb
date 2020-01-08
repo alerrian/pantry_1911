f38fe099369f43ea538711a01ca8bd81c7b65122
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-08 10:37:11 -0700

    Write existence and attribute tests for pantry

---
+++ test/pantry_test.rb
@@ -0,0 +1,24 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require_relative '../lib/pantry'
+require_relative '../lib/recipe'
+require_relative '../lib/ingredient'
+
+class RecipeTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @ingredient1 = Ingredient.new('Cheese', 'C', 100)
+    @ingredient2 = Ingredient.new('Macaroni', 'oz', 30)
+    @recipe = Recipe.new('Mac and Cheese')
+  end
+
+  def test_a_pantry_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_a_pantry_hash_attributes
+    expected = {}
+
+    assert_equal expected, @pantry.stock
+  end
+end
